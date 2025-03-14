10b56abdf21e970d9f391354af5e2317b4a8ddbc
Andy <andy.ruan2005@gmail.com>
2025-03-14 22:09:16 +1100

    made backend interact with frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import React, {useState, useEffect} from 'react'
 import axios from "axios";
 
 function App() {
+  const [array, setArray] = useState([])
 
   const fetchAPI = async () => {
     const response = await axios.get("http://127.0.0.1:5000/members")
     console.log(response.data.members)
+    setArray(response.data.members);
   }
 
   useEffect(() => {
@@ -13,7 +15,13 @@ function App() {
   }, []);
 
   return (
-    <div>App</div>
+    <div>
+      <p>
+        {array.map((user, index) => (
+          <span key={index}>{user}</span>
+          ))}
+      </p>
+    </div>
   )
 }
 
