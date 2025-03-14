98fe561138f60ac6a9858a0480762c6826cd76a5
Andy <andy.ruan2005@gmail.com>
2025-03-14 22:01:59 +1100

    connected backend to frontend

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
+
+@app.route("/members", methods=['GET'])
+def members():
+    return jsonify(
+        {"members": ["Member1", "Member2", "Member3"]}
+    )
 
 @app.route('/')
 def index():
