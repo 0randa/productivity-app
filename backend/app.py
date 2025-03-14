8418d15e0b399341dc1651cdbda01a027e8585bb
im-json <gravellguy@gmail.com>
2025-03-14 21:43:01 +1100

    testing testing 123

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "We are the goat team!"
+    return "We are the goat team! Heheheha!"
 
 if __name__ == "__main__":
     app.run(debug=True)
