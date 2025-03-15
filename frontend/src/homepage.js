bb4b815d5765564eaa856268817b238bd556ae82
im-json <gravellguy@gmail.com>
2025-03-16 05:47:12 +1100

    Added major changes to homepage.js, implememted pomodoro timer logic and removed redundant code

--- frontend/src/homepage.js
+++ frontend/src/homepage.js
@@ -16,32 +16,242 @@ closeBtn.addEventListener("click", function () {
 });
 
 // set initial count
-let count = 0;
+let defaultHours = 0;
+let defaultMinutes = 25;
+let defaultSeconds = 0;
+hoursLeft = defaultHours;
+minutesLeft = defaultMinutes;
+secondsLeft = defaultSeconds;
+let timerOn = 0;
+let timerReset = 0;
+let interval;
 
 // select value and buttons
-const value = document.querySelector('#value');
+const timerHours = document.querySelector('#timer-hours');
+const timerColonLeft = document.querySelector('#timer-colon-left');
+const timerMinutes = document.querySelector('#timer-minutes');
+const timerColonRight = document.querySelector('#timer-colon-right');
+const timerSeconds = document.querySelector('#timer-seconds');
 const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function (btn) {
-    btn.addEventListener("click", function (e){
+    btn.addEventListener("click", function (e) {
         const styles = e.currentTarget.classList;
-        if (styles.contains('decrease')) {
-            count--;
-        } else if (styles.contains('increase')) {
-            count++;
-        } else {
-            count = 0;
+        
+        if (styles.contains('default')) {
+            defaultHours = hoursLeft;
+            defaultMinutes = minutesLeft;
+            defaultSeconds = secondsLeft;
+        }  else if (styles.contains('start')) {
+            if (!interval) {
+                interval = setInterval(timer, 1000);
+            }
+            timerOn = 1;
+        } else if (styles.contains('pause')) {
+            if (timerOn ? timerOn = 0 : timerOn = 1);
+        } else if (styles.contains('reset')) {
+            hoursLeft = defaultHours;
+            minutesLeft = defaultMinutes;
+            secondsLeft = defaultSeconds;
+            if (minutesLeft < 10) {
+                timerColonLeft.textContent = ":0";
+            } else {
+                timerColonLeft.textContent = ":";
+            }
+            if (secondsLeft < 10) {
+                timerColonRight.textContent = ":0";
+            } else {
+                timerColonRight.textContent = ":";
+            }
+            timerHours.textContent = hoursLeft;
+            timerMinutes.textContent = minutesLeft;
+            timerSeconds.textContent = secondsLeft;
+            timerOn = 0;
+        } else if (styles.contains('clear')) {
+            hoursLeft = 0;
+            minutesLeft = 0;
+            secondsLeft = 0;
+            timerColonLeft.textContent = ":0";
+            timerColonRight.textContent = ":0";
+            timerHours.textContent = hoursLeft;
+            timerMinutes.textContent = minutesLeft;
+            timerSeconds.textContent = secondsLeft;
+        } else if (styles.contains('+hour')) {
+            if (hoursLeft == 99) {
+                hoursLeft = 0;
+                minutesLeft = 0;
+                secondsLeft = 0;
+            } else {
+                hoursLeft++;
+            }
+            if (minutesLeft < 10) {
+                timerColonLeft.textContent = ":0";
+            } else {
+                timerColonLeft.textContent = ":";
+            }
+            if (secondsLeft < 10) {
+                timerColonRight.textContent = ":0";
+            } else {
+                timerColonRight.textContent = ":";
+            }
+            timerHours.textContent = hoursLeft;
+            timerMinutes.textContent = minutesLeft;
+            timerSeconds.textContent = secondsLeft;
+        } else if (styles.contains('-hour')) {
+            if (hoursLeft == 0) {
+                minutesLeft = 0;
+                secondsLeft = 0;
+            } else {
+                hoursLeft--;
+            }
+            if (minutesLeft < 10) {
+                timerColonLeft.textContent = ":0";
+            } else {
+                timerColonLeft.textContent = ":";
+            }
+            if (secondsLeft < 10) {
+                timerColonRight.textContent = ":0";
+            } else {
+                timerColonRight.textContent = ":";
+            }
+            timerHours.textContent = hoursLeft;
+            timerMinutes.textContent = minutesLeft;
+            timerSeconds.textContent = secondsLeft;
+        } else if (styles.contains('+minute')) {
+            if (hoursLeft == 99 && minutesLeft == 59) {
+                hoursLeft = 0;
+                minutesLeft = 0;
+                secondsLeft = 0;
+            } else if (minutesLeft == 59) {
+                hoursLeft++;
+                minutesLeft = 0;
+            } else {
+                minutesLeft++;
+            }
+            if (minutesLeft < 10) {
+                timerColonLeft.textContent = ":0";
+            } else {
+                timerColonLeft.textContent = ":";
+            }
+            if (secondsLeft < 10) {
+                timerColonRight.textContent = ":0";
+            } else {
+                timerColonRight.textContent = ":";
+            }
+            timerHours.textContent = hoursLeft;
+            timerMinutes.textContent = minutesLeft;
+            timerSeconds.textContent = secondsLeft;
+        } else if (styles.contains('-minute')) {
+            if (hoursLeft == 0 && minutesLeft == 0) {
+                secondsLeft = 0;
+            } else if (minutesLeft == 0) {
+                hoursLeft--;
+                minutesLeft = 59;
+            } else {
+                minutesLeft--;
+            }
+            if (minutesLeft < 10) {
+                timerColonLeft.textContent = ":0";
+            } else {
+                timerColonLeft.textContent = ":";
+            }
+            if (secondsLeft < 10) {
+                timerColonRight.textContent = ":0";
+            } else {
+                timerColonRight.textContent = ":";
+            }
+            timerHours.textContent = hoursLeft;
+            timerMinutes.textContent = minutesLeft;
+            timerSeconds.textContent = secondsLeft;
+        } else if (styles.contains('+second')) {
+            if (hoursLeft == 99 && minutesLeft == 59 &&
+                secondsLeft == 59) {
+                hoursLeft = 0;
+                minutesLeft = 0;
+                secondsLeft = 0;
+            } else if (minutesLeft == 59 && secondsLeft == 59) {
+                hoursLeft++;
+                minutesLeft = 0;
+                secondsLeft = 0;
+            } else if (secondsLeft == 59) {
+                minutesLeft++;
+                secondsLeft = 0;
+            } else {
+                secondsLeft++;
+            }
+            if (minutesLeft < 10) {
+                timerColonLeft.textContent = ":0";
+            } else {
+                timerColonLeft.textContent = ":";
+            }
+            if (secondsLeft < 10) {
+                timerColonRight.textContent = ":0";
+            } else {
+                timerColonRight.textContent = ":";
+            }
+            timerHours.textContent = hoursLeft;
+            timerMinutes.textContent = minutesLeft;
+            timerSeconds.textContent = secondsLeft;
+        } else if (styles.contains('-second')) {
+            if (hoursLeft == 0 && minutesLeft == 0 &&
+                secondsLeft == 0) {
+                return;
+            } else if (minutesLeft == 0 && secondsLeft == 0) {
+                hoursLeft--;
+                minutesLeft = 59;
+                secondsLeft = 59;
+            } else if (secondsLeft == 0) {
+                minutesLeft--;
+                secondsLeft = 59;
+            } else {
+                secondsLeft--;
+            }
+            if (minutesLeft < 10) {
+                timerColonLeft.textContent = ":0";
+            } else {
+                timerColonLeft.textContent = ":";
+            }
+            if (secondsLeft < 10) {
+                timerColonRight.textContent = ":0";
+            } else {
+                timerColonRight.textContent = ":";
+            }
+            timerHours.textContent = hoursLeft;
+            timerMinutes.textContent = minutesLeft;
+            timerSeconds.textContent = secondsLeft;
         }
-        if (count > 0) {
-            value.style.color = "green";
+        function timer() {
+            if (!timerOn) {
+                timer()
+            }
+            if (hoursLeft == 0 && minutesLeft == 0 &&
+                secondsLeft == 0) {
+                timerOn = 0;
+                return;
+            } else if (minutesLeft == 0 && secondsLeft == 0) {
+                hoursLeft--;
+                minutesLeft = 59;
+                secondsLeft = 59;
+            } else if (secondsLeft == 0) {
+                minutesLeft--;
+                secondsLeft = 59;
+            } else {
+                secondsLeft--;
+            }
+            if (minutesLeft < 10) {
+                timerColonLeft.textContent = ":0";
+            } else {
+                timerColonLeft.textContent = ":";
+            }
+            if (secondsLeft < 10) {
+                timerColonRight.textContent = ":0";
+            } else {
+                timerColonRight.textContent = ":";
+            }
+            timerHours.textContent = hoursLeft;
+            timerMinutes.textContent = minutesLeft;
+            timerSeconds.textContent = secondsLeft;
         }
-        if (count < 0) {
-            value.style.color = "red";
-        }
-        if (count == 0) {
-            value.style.color = "#222";
-        }
-        value.textContent = count;
     });
 });
 
@@ -55,26 +265,10 @@ modal.classList.add("open-modal");
 
 modalLoginBtn.addEventListener("click", function () {
 
-    setInterval(function () {
-        modal.classList.add("open-modal");
-    }, 200);
-    for (let i = 1; i < 1000; i++) {
-        setTimeout(function () {
-            modal.classList.remove("open-modal");
-        }, i * 400);
-    }
 });
 
 modalRegisterBtn.addEventListener("click", function () {
 
-    setInterval(function () {
-        modal.classList.add("open-modal");
-    }, 200);
-    for (let i = 1; i < 1000; i++) {
-        setTimeout(function () {
-            modal.classList.remove("open-modal");
-        }, i * 400);
-    }
 });
 
 modalGuestBtn.addEventListener("click", function () {
