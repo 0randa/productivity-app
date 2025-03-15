25987c476b297b8ae6eedf6114e21e584d743f54
Andy <andy.ruan2005@gmail.com>
2025-03-15 12:10:16 +1100

    made task class

--- backend/src/classes/task.py
+++ backend/src/classes/task.py
@@ -1,5 +1,15 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from datetime import datetime, date
+from typing import List, Optional
 
 @dataclass
 class Task:
-    pass
+    task_id: int
+    task: str
+    tags: List[str]
+    date: date = field(default_factory=list)
+    start_time: datetime = field(default_factory=datetime.now)
+    end_time: Optional[datetime] = None
+
+    def end_task(self):
+        self.end_time = datetime.now()
